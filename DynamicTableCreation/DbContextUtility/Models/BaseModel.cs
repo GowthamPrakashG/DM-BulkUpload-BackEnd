bdd9fa944916ab28e1650d341fd8cfeb039d8055
Sathish_Ravichandran <10decoders@DESKTOP-RD0I1G3>
2023-12-27 16:19:21 +0530

    Adding DbContextUtilityClass

---
+++ DynamicTableCreation/DbContextUtility/Models/BaseModel.cs
@@ -0,0 +1,12 @@
+﻿namespace DbContextUtility.Models
+{
+    public class BaseModel
+    {
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public int CreatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; } = DateTime.Now;
+        public int UpdatedBy { get; set; }
+    }
+
+}
+
---
+++ DynamicTableCreation/DbContextUtility/Models/LogChild.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DbContextUtility.Models
+{
+    public class LogChild
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [ForeignKey("ParentID")]
+        public int ParentID { get; set; }
+        public LogParent Parent { get; set; }
+        public string ErrorMessage { get; set; }
+        public string Filedata { get; set; }
+        public string ErrorRowNumber { get; set; }
+    }
+}
